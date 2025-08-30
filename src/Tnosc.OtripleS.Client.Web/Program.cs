d870579d53c3216fc8089f1d21ddf81f19d64e8f
ahedfi <ahmed.hedfi@gmail.com>
2025-08-30 10:18:05 +0100

    PROJECTS: Setup

---
+++ src/Tnosc.OtripleS.Client.Web/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
+using Tnosc.OtripleS.Client.Web;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddFluentUIComponents();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Tnosc.OtripleS.Client.Web.Client._Imports).Assembly);
+
+await app.RunAsync();
