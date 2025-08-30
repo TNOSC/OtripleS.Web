d4beb10c930d3d841fdfe9040a6ce0afdaa460e8
ahedfi <ahmed.hedfi@gmail.com>
2025-08-30 12:04:19 +0100

    BUILD: Add build script

---
+++ devops/Tnosc.OtripleS.Client.Web.Build/Program.cs
@@ -0,0 +1,10 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+
+using Tnosc.OtripleS.Server.Build.Services;
+
+ScriptGenerationService.GenerateBuildScript();
