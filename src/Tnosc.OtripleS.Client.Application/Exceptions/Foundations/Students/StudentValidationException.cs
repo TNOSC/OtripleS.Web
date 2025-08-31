fb2ce20726c773c684c1f6cd707f48bcbeec496f
ahedfi <ahmed.hedfi@gmail.com>
2025-09-01 00:07:59 +0100

    ShouldThrowValidationExceptionOnRegisterWhenStudentIsNullAndLogItAsync -> FAIL

---
+++ src/Tnosc.OtripleS.Client.Application/Exceptions/Foundations/Students/StudentValidationException.cs
@@ -0,0 +1,20 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Xeptions;
+
+namespace Tnosc.OtripleS.Client.Application.Exceptions.Foundations.Students;
+
+public sealed class StudentValidationException : Xeption
+{
+    public StudentValidationException(
+        string message,
+        Xeption innerException)
+       : base(
+           message: message,
+           innerException: innerException)
+    { }
+}
---
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Services/Foundations/Register/StudentServiceTests.validations.cs
@@ -0,0 +1,51 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using NSubstitute;
+using Shouldly;
+using Tnosc.OtripleS.Client.Application.Exceptions.Foundations.Students;
+using Tnosc.OtripleS.Client.Domain.Students;
+using Xeptions;
+using Xunit;
+
+namespace Tnosc.OtripleS.Client.Web.Tests.Unit.Services.Foundations;
+
+public partial class StudentServiceTests
+{
+    [Fact]
+    public async Task ShouldThrowValidationExceptionOnRegisterWhenStudentIsNullAndLogItAsync()
+    {
+        // given
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        Student invalidStudent = null;
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+        var nullStudentException = new NullStudentException(message: "The student is null.");
+
+        var expectedStudentValidationException =
+            new StudentValidationException(
+                message: "Invalid input, fix the errors and try again.",
+                innerException: nullStudentException);
+        // when
+#pragma warning disable CS8604 // Possible null reference argument.
+        ValueTask<Student> registerStudentTask =
+            _studentService.RegisterStudentAsync(student: invalidStudent);
+#pragma warning restore CS8604 // Possible null reference argument.
+
+        // then
+        await Assert.ThrowsAsync<StudentValidationException>(() =>
+         registerStudentTask.AsTask());
+
+        _loggingBrokerMock.Received(requiredNumberOfCalls: 1)
+            .LogError(Arg.Is<Xeption>(actualException =>
+                actualException.SameExceptionAs(expectedStudentValidationException)));
+
+        _apiBrokerMock
+            .ReceivedCalls()
+            .ShouldBeEmpty();
+
+    }
+}
