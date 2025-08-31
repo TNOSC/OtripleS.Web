f4e8c35a0428291509a4a5984361c1a6fb125196
ahedfi <ahmed.hedfi@gmail.com>
2025-09-01 00:30:59 +0100

    ShouldThrowValidationExceptionOnRegisterWhenStudentIsInvalidAndLogItAsync -> FAIL

--- tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Services/Foundations/Register/StudentServiceTests.validations.cs
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Services/Foundations/Register/StudentServiceTests.validations.cs
@@ -4,6 +4,7 @@
 // Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
 // ----------------------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using NSubstitute;
 using Shouldly;
@@ -47,5 +48,89 @@ public partial class StudentServiceTests
             .ReceivedCalls()
             .ShouldBeEmpty();
 
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("   ")]
+    public async Task ShouldThrowValidationExceptionOnRegisterWhenStudentIsInvalidAndLogItAsync(string? invalidText)
+    {
+        // given
+#pragma warning disable CS8601 // Possible null reference assignment.
+        var invalidStudent = new Student()
+        {
+            Id = Guid.Empty,
+            UserId = invalidText,
+            IdentityNumber = invalidText,
+            FirstName = invalidText,
+            LastName = invalidText,
+        };
+#pragma warning restore CS8601 // Possible null reference assignment.
+
+        var invalidStudentException =
+            new InvalidStudentException(message: "Invalid student. Please fix the errors and try again.");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.Id),
+            values: "Id is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.UserId),
+            values: "Text is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.IdentityNumber),
+            values: "Text is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.FirstName),
+            values: "Text is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.LastName),
+            values: "Text is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.BirthDate),
+            values: "Date is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.CreatedBy),
+            values: "Id is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.UpdatedBy),
+            values: "Id is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.CreatedDate),
+            values: "Date is required");
+
+        invalidStudentException.AddData(
+            key: nameof(Student.UpdatedDate),
+            values: "Date is required");
+
+        var expectedStudentValidationException =
+            new StudentValidationException(
+                message: "Invalid input, fix the errors and try again.",
+                innerException: invalidStudentException);
+
+        // when
+        ValueTask<Student> registerStudentTask =
+            _studentService.RegisterStudentAsync(student: invalidStudent);
+
+        // then
+        await Assert.ThrowsAsync<StudentValidationException>(() =>
+            registerStudentTask.AsTask());
+
+      
+        _loggingBrokerMock.Received(requiredNumberOfCalls: 1)
+            .LogError(Arg.Is<Xeption>(actualException =>
+                actualException.SameExceptionAs(expectedStudentValidationException)));
+
+        _apiBrokerMock
+            .ReceivedCalls()
+            .ShouldBeEmpty();
     }
 }
