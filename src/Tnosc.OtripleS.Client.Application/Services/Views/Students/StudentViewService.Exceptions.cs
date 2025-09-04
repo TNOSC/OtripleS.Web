3e6927ec57d4fb6d34113d910a4f433046567fe8
ahedfi <ahmed.hedfi@gmail.com>
2025-09-04 23:55:49 +0100

    ShouldThrowValidationExceptionOnRegisterIfStudentViewIsNullAndLogItAsync -> PASS

---
+++ src/Tnosc.OtripleS.Client.Application/Services/Views/Students/StudentViewService.Exceptions.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Tnosc.OtripleS.Client.Application.Exceptions.Views.Students;
+using Tnosc.OtripleS.Client.Application.ViewModels.Students;
+using Xeptions;
+
+namespace Tnosc.OtripleS.Client.Application.Services.Views.Students;
+
+public partial class StudentViewService
+{
+    private delegate ValueTask<StudentView> ReturningStudentViewFunction();
+
+    private async ValueTask<StudentView> TryCatch(ReturningStudentViewFunction returningStudentViewFunction)
+    {
+        try
+        {
+            return await returningStudentViewFunction();
+        }
+        catch (NullStudentViewException nullStudentViewException)
+        {
+            throw CreateAndLogValidationException(nullStudentViewException);
+        }
+    }
+
+    private StudentViewValidationException CreateAndLogValidationException(Xeption exception)
+    {
+        var studentViewValidationException = new StudentViewValidationException(
+            message: "Invalid input, fix the errors and try again.",
+            innerException: exception);
+        _loggingBroker.LogError(studentViewValidationException);
+
+        return studentViewValidationException;
+    }
+}
---
+++ src/Tnosc.OtripleS.Client.Application/Services/Views/Students/StudentViewService.Validations.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Tnosc.OtripleS.Client.Application.Exceptions.Views.Students;
+using Tnosc.OtripleS.Client.Application.ViewModels.Students;
+
+namespace Tnosc.OtripleS.Client.Application.Services.Views.Students;
+
+public partial class StudentViewService
+{
+    private static void ValidateStudentView(StudentView studentView)
+    {
+        if (studentView is null)
+        {
+            throw new NullStudentViewException( message: "The student is null.");
+        }
+    }
+}
