f4021a20b84f2fde04851bf1d86fff93fe4ca584
ahedfi <ahmed.hedfi@gmail.com>
2025-09-14 11:16:03 +0100

    ShouldNavigateToRoute -> FAIL

---
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Bases/AppComponentTests.Render.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Linq;
+using NSubstitute;
+using Shouldly;
+using Tnosc.Lib.Client.Web.Bases;
+using Xunit;
+
+namespace Tnosc.OtripleS.Client.Web.Tests.Unit.Components.Bases;
+
+public partial class AppComponentTests
+{
+    [Fact]
+    public void ShouldNavigateToRoute()
+    {
+        // given
+        string randomRoute = GetRandomRoute();
+        string inputRoute = randomRoute;
+
+        // when
+        _renderedAppComponent =
+            RenderComponent<AppComponent>();
+
+        _renderedAppComponent.Instance.NavigateTo(inputRoute);
+
+        // then
+        _navigationBrokerMock.Received(requiredNumberOfCalls: 1)
+             .NavigateTo(route: inputRoute);
+
+        _navigationBrokerMock
+            .ReceivedCalls()
+            .Count()
+            .ShouldBe(expected: 1);
+    }
+}
---
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Bases/AppComponentTests.cs
@@ -0,0 +1,36 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Bunit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using Tnosc.Lib.Client.Web.Bases;
+using Tnosc.Lib.Client.Web.Navigations;
+using Tynamix.ObjectFiller;
+
+namespace Tnosc.OtripleS.Client.Web.Tests.Unit.Components.Bases;
+
+public partial class AppComponentTests : TestContext
+{
+    private readonly INavigationBroker _navigationBrokerMock;
+    private IRenderedComponent<AppComponent> _renderedAppComponent = null!;
+
+    public AppComponentTests()
+    {
+        _navigationBrokerMock = Substitute.For<INavigationBroker>();
+        IWebHostEnvironment envMock = Substitute.For<IWebHostEnvironment>();
+        IConfiguration configMock = Substitute.For<IConfiguration>();
+        Services.AddScoped(services => _navigationBrokerMock);
+        Services.AddSingleton(envMock);
+        Services.AddSingleton(configMock);
+        Services.AddServerSideBlazor();
+    }
+
+    private static string GetRandomRoute() =>
+           new RandomUrl().GetValue();
+}
