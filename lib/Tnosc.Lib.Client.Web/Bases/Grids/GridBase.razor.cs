6285c947e3b7573bcd28d00af9e95afb3f4046ce
ahedfi <ahmed.hedfi@gmail.com>
2025-09-16 20:43:31 +0100

    Add GridBase component

---
+++ lib/Tnosc.Lib.Client.Web/Bases/Grids/GridBase.razor.cs
@@ -0,0 +1,16 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Tnosc.Lib.Client.Web.Bases.Grids;
+
+public partial class GridBase<T> : ComponentBase
+{
+    [Parameter]
+    public IEnumerable<T> DataSource { get; set; } = [];
+}
