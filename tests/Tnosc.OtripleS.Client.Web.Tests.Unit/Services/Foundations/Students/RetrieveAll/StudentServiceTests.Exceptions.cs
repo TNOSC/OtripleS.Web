46f63a9cd91b65e349d6f1f6c23027562af0efcf
ahedfi <ahmed.hedfi@gmail.com>
2025-09-16 21:54:56 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfCriticalErrorOccursAndLogItAsync -> FAIL

---
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Services/Foundations/Students/RetrieveAll/StudentServiceTests.Exceptions.cs
@@ -0,0 +1,66 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using NSubstitute.ExceptionExtensions;
+using Shouldly;
+using Tnosc.OtripleS.Client.Application.Exceptions.Foundations.Students;
+using Tnosc.OtripleS.Client.Domain.Students;
+using Xeptions;
+using Xunit;
+
+namespace Tnosc.OtripleS.Client.Web.Tests.Unit.Services.Foundations.Students;
+
+public partial class StudentServiceTests
+{
+    [Theory]
+    [MemberData(nameof(CriticalApiException))]
+    public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfCriticalErrorOccursAndLogItAsync(
+       Exception httpResponseCriticalException)
+    {
+        // given
+        var failedStudentCriticalDependencyException =
+            new FailedStudentCriticalDependencyException(
+                message: "Failed student critical dependency error occurred, please contact support.",
+                innerException: httpResponseCriticalException);
+
+        var expectedDependencyValidationException =
+            new StudentDependencyException(
+                message: "Student dependency error occurred, please contact support.",
+                innerException: failedStudentCriticalDependencyException);
+
+        _apiBrokerMock.GetAllStudentsAsync()
+            .ThrowsAsync(failedStudentCriticalDependencyException);
+
+        // when
+        ValueTask<IEnumerable<Student>> retrievedStudentTask =
+            _studentService.RetrieveAllStudentsAsync();
+
+        // then
+        await Assert.ThrowsAsync<StudentDependencyException>(() =>
+            retrievedStudentTask.AsTask());
+
+        _loggingBrokerMock.Received(requiredNumberOfCalls: 1)
+            .LogCritical(Arg.Is<Exception>(actualException =>
+                actualException.SameExceptionAs(expectedDependencyValidationException)));
+
+        await _apiBrokerMock
+            .Received(requiredNumberOfCalls: 1)
+            .GetAllStudentsAsync();
+
+       _apiBrokerMock.ReceivedCalls()
+            .Count()
+            .ShouldBe(expected: 1);
+
+        _loggingBrokerMock.ReceivedCalls()
+            .Count()
+            .ShouldBe(expected: 1);
+    }
+}
