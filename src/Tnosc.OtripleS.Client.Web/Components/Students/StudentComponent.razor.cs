b46466784d3a18d08d0447d673c68ba173a7b122
ahedfi <ahmed.hedfi@gmail.com>
2025-09-16 20:44:10 +0100

    Add Student component

---
+++ src/Tnosc.OtripleS.Client.Web/Components/Students/StudentComponent.razor.cs
@@ -0,0 +1,13 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Tnosc.Lib.Client.Web.Bases.Components;
+
+namespace Tnosc.OtripleS.Client.Web.Components.Students;
+
+public partial class StudentComponent : AppViewComponent
+{
+}
