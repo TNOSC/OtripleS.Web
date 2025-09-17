7c8d14237857c33a028dff9c49fd779d2ec4c7a0
ahedfi <ahmed.hedfi@gmail.com>
2025-09-17 13:52:45 +0100

    ShouldRenderTeachersAsync -> FAIL

--- tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Students/StudentsComponentTests.Render.cs
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Students/StudentsComponentTests.Render.cs
@@ -1,5 +1,10 @@
-﻿using Shouldly;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NSubstitute;
+using Shouldly;
 using Tnosc.Lib.Client.Web.Enums;
+using Tnosc.OtripleS.Client.Application.ViewModels.Students;
 using Tnosc.OtripleS.Client.Web.Components.Students;
 using Xunit;
 
@@ -25,5 +30,52 @@ public partial class StudentsComponentTests
         initialTeachersComponent.StudentGrid.ShouldBeNull();
         initialTeachersComponent.ErrorMessage.ShouldBeNull();
         initialTeachersComponent.ErrorLabel.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task ShouldRenderTeachersAsync()
+    {
+        // given
+        ComponentState expectedState =
+            ComponentState.Content;
+
+        IEnumerable<StudentView> randomStudentViews =
+            CreateRandomStudentViews();
+
+        IEnumerable<StudentView> retrievedStudentViews =
+            randomStudentViews;
+
+        IEnumerable<StudentView> expectedStudentViews =
+            retrievedStudentViews;
+
+        _studentViewServiceMock.RetrieveAllStudentViewsAsync()
+            .Returns(retrievedStudentViews);
+
+        // when
+        _renderedStudentsComponent =
+            RenderComponent<StudentsComponent>();
+
+        // then
+        _renderedStudentsComponent.Instance.State
+            .ShouldBe(expectedState);
+
+        _renderedStudentsComponent.Instance.StudentViews
+            .ShouldBeEquivalentTo(expectedStudentViews);
+
+        _renderedStudentsComponent.Instance.StudentGrid
+            .ShouldNotBeNull();
+
+        _renderedStudentsComponent.Instance.StudentGrid.DataSource
+            .ShouldBeEquivalentTo(expectedStudentViews);
+
+        _renderedStudentsComponent.Instance.ErrorMessage.
+            ShouldBeNull();
+
+        _renderedStudentsComponent.Instance.ErrorLabel.
+            ShouldBeNull();
+
+        await _studentViewServiceMock.Received(requiredNumberOfCalls: 1)
+            .RetrieveAllStudentViewsAsync();
+        _studentViewServiceMock.ReceivedCalls().Count().ShouldBe(1);
     }
 }
--- tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Students/StudentsComponentTests.cs
+++ tests/Tnosc.OtripleS.Client.Web.Tests.Unit/Components/Students/StudentsComponentTests.cs
@@ -1,18 +1,22 @@
-﻿using Bunit;
+﻿using System;
+using System.Collections.Generic;
+using Bunit;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.FluentUI.AspNetCore.Components;
 using NSubstitute;
 using Tnosc.OtripleS.Client.Application.Services.Views.Students;
+using Tnosc.OtripleS.Client.Application.ViewModels.Students;
 using Tnosc.OtripleS.Client.Web.Components.Students;
+using Tynamix.ObjectFiller;
 
 namespace Tnosc.OtripleS.Client.Web.Tests.Unit.Components.Students;
 
 public partial class StudentsComponentTests : TestContext
 {
     private readonly IStudentViewService _studentViewServiceMock;
-
+    private IRenderedComponent<StudentsComponent> _renderedStudentsComponent = null!;
     public StudentsComponentTests()
     {
         _studentViewServiceMock = Substitute.For<IStudentViewService>();
@@ -26,5 +30,21 @@ public partial class StudentsComponentTests : TestContext
         JSInterop.Mode = JSRuntimeMode.Loose;
         JSInterop.SetupModule("./_content/Microsoft.FluentUI.AspNetCore.Components/Components/TextField/FluentTextField.razor.js?v=4.12.1.25197");
         JSInterop.SetupVoid("setControlAttribute", _ => true);
+    }
+
+    private static IEnumerable<StudentView> CreateRandomStudentViews() =>
+          CreateStudentFiller().Create(count: GetRandomNumber());
+
+    private static int GetRandomNumber() =>
+          new IntRange(min: 2, max: 10).GetValue();
+
+    private static Filler<StudentView> CreateStudentFiller()
+    {
+        var filler = new Filler<StudentView>();
+
+        filler.Setup()
+            .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+        return filler;
     }
 }
