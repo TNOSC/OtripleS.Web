70362215a6e544b82d55186a7236576a7f382e25
ahedfi <ahmed.hedfi@gmail.com>
2025-09-23 19:11:06 +0100

    Add ProgressLoaderBase

---
+++ lib/Tnosc.Lib.Client.Web/Bases/Loaders/ProgressLoaderBase.razor.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------------
+// Copyright (c) Tunisian .NET Open Source Community (TNOSC). All rights reserved.
+// This code is provided by TNOSC and is freely available under the MIT License.
+// Author: Ahmed HEDFI (ahmed.hedfi@gmail.com)
+// ----------------------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Components;
+
+namespace Tnosc.Lib.Client.Web.Bases.Loaders;
+
+public partial class ProgressLoaderBase : ComponentBase
+{
+    [Parameter]
+    public string ProgressText { get; set; } = "Loading...";
+
+    [Parameter]
+    public int Columns { get; set; } = 5;
+}
