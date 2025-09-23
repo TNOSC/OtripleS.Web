e7f7b75308c2dad673dcef2853621741f1fd2a33
ahedfi <ahmed.hedfi@gmail.com>
2025-09-23 19:10:37 +0100

    Add new properties to GridBase

--- lib/Tnosc.Lib.Client.Web/Bases/Grids/GridBase.razor.cs
+++ lib/Tnosc.Lib.Client.Web/Bases/Grids/GridBase.razor.cs
@@ -6,6 +6,7 @@
 
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.FluentUI.AspNetCore.Components;
 
@@ -14,14 +15,29 @@ namespace Tnosc.Lib.Client.Web.Bases.Grids;
 public partial class GridBase<TItem> : ComponentBase
 {
     [Parameter]
-    public IEnumerable<TItem> DataSource { get; set; } = Enumerable.Empty<TItem>();
+    public IEnumerable<TItem>? DataSource { get; set; }
 
     [Parameter]
     public RenderFragment? Columns { get; set; }
 
+    protected override async Task OnParametersSetAsync()
+    {
+        if (!ReferenceEquals(DataSource, _lastDataSource))
+        {
+            _lastDataSource = DataSource;
+            await InvokeAsync(StateHasChanged);
+        }
+    }
+
     private readonly PaginationState pagination = new() { ItemsPerPage = 10 };
 
     private IQueryable<TItem> PagedData =>
-        DataSource.Skip(pagination.CurrentPageIndex * pagination.ItemsPerPage)
-                  .Take(pagination.ItemsPerPage).AsQueryable();
+        (DataSource ?? Enumerable.Empty<TItem>())
+            .Skip(pagination.CurrentPageIndex * pagination.ItemsPerPage)
+            .Take(pagination.ItemsPerPage)
+            .AsQueryable();
+
+    private bool IsLoading => DataSource == null || !DataSource.Any();
+
+    private IEnumerable<TItem>? _lastDataSource;
 }
